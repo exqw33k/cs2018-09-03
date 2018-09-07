a7c4e11104ea8d833d5b1ca7c2cbf442c9b402f4
exqw33k <exqweek1@yandex.ru>
2018-09-07 12:31:07 +0300

    start lesson3

--- src/by/it/ushakov/lesson03/TaskA2.java
+++ src/by/it/ushakov/lesson03/TaskA2.java
@@ -21,5 +21,12 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
-
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        String a=("У лукаморья дуб зеленый");
+        String b=("Злотая цепь на дубе том");
+        String c=("И днем и ночью кот ученый");
+        String d=("Все ходит по цепи кругом");
+        System.out.println (a+"\n"+b+c+d);
+    }
 }
